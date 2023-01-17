9d08b9c0514a9b0da67b5569a3d95c08a72c38f7
bso <cbsubx1@gmail.com>
2023-01-16 19:46:39 -0700

    Refined routines to get GUIDs

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # PowerScheme Switching
 
 Windows Batch file with subtoutines to 
-- save current Power Scheme
-- switch to Performance Scheme
-- return to saved scheme
+- save current power mode
+- get the GUIDs for the three modes: Performance, Balanced and Power saver
+- switch to the different modes
+- return to saved mode
--- powerschemes.bat
+++ powerschemes.bat
@@ -6,13 +6,29 @@ REM ========= Main ==========
 Call :get_current_pwrscheme var_saved_pwrscheme
 echo Current Power Scheme: %var_saved_pwrscheme%
 
-Call :get_performanceGUID var_performanceGUID
-echo Power: %var_performanceGUID%
+Call :get_powerschemeGUIDs var_performanceGUID var_balancedGUID var_PsaverGUID
+echo GUID for Performance: %var_performanceGUID%
+echo GUID for Balanced: %var_balancedGUID%
+echo GUID for Saver: %var_PsaverGUID%
 
+pause
+
+echo Switching to Performance mode...
 Call :set_powercfg %var_performanceGUID%
 
 pause
 
+echo Switching to Power Saver mode...
+Call :set_powercfg %var_PsaverGUID%
+
+pause
+
+echo Switching to Balanced Power mode...
+Call :set_powercfg %var_balancedGUID%
+
+pause
+
+echo Returning to original power mode...
 Call :set_powercfg %var_saved_pwrscheme%
 
 goto end_program
@@ -24,12 +40,54 @@ REM ========= Get current powerscheme GUID =========
     )
 goto :eof
 
+REM ========= Get powerscheme GUIDs =========
+:get_powerschemeGUIDs
+    for /f "tokens=3,4,* delims= " %%i in ('powercfg /L') DO (
+       if "%%i"=="GUID:" (
+           set tmp=%%k
+           if not "!tmp!"=="!tmp:performance=!" (
+               set %1=%%j
+           )
+           if not "!tmp!"=="!tmp:Balanced=!" (
+               set %2=%%j
+           )
+           if not "!tmp!"=="!tmp:saver=!" (
+               set %3=%%j
+           )
+       )
+    )
+goto :eof
+
 REM ========= Get Peformance powerscheme GUID =========
 :get_performanceGUID
     for /f "tokens=3,4,* delims= " %%i in ('powercfg /L') DO (
        if "%%i"=="GUID:" (
            set tmp=%%k
            if not "!tmp!"=="!tmp:performance=!" (
+               set %1=%%j
+           )
+       )
+    )
+goto :eof
+
+REM ========= Get Power Saver powerscheme GUID =========
+:get_PsaverGUID
+    for /f "tokens=3,4,* delims= " %%i in ('powercfg /L') DO (
+       if "%%i"=="GUID:" (
+           set tmp=%%k
+           if not "!tmp!"=="!tmp:saver=!" (
+               set %1=%%j
+           )
+       )
+    )
+goto :eof
+
+REM ========= Get Balanced powerscheme GUID =========
+:get_PBalancedGUID
+    for /f "tokens=3,4,* delims= " %%i in ('powercfg /L') DO (
+       if "%%i"=="GUID:" (
+           set tmp=%%k
+           if not "!tmp!"=="!tmp:Balanced=!" (
                set %1=%%j
            )
        )
